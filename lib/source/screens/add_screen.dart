8892948e01dc63c3563a8122eb40a79471fc92f3
norhum/gobouman <devgobouman@gmail.com>
2022-12-10 11:23:09 +0900

    とりあえずボタン表示

--- lib/source/screens/add_screen.dart
+++ lib/source/screens/add_screen.dart
@@ -12,44 +12,49 @@ class _AddScreenState extends State<AddScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AddScene'),
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Container(
-                height: 20,
-              ),
-              const NHInputField(labelName: 'English'),
-              Container(
-                height: 20,
-              ),
-              const NHInputField(labelName: '日本語'),
-              // Padding(
-              //   padding: const EdgeInsets.all(60),
-              //   child: Row(
-              //     children: [
-              //       Expanded(
-              //           flex: 3,
-              //           child: OutlinedButton(
-              //               onPressed: () {}, child: Text('キャンセル'))),
-              //       Spacer(
-              //         flex: 1,
-              //       ),
-              //       Expanded(
-              //           flex: 3,
-              //           child:
-              //               ElevatedButton(onPressed: () {}, child: Text('保存'))),
-              //     ],
-              //   ),
-              // )
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('AddScene'),
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 20,
+                    ),
+                    const NHInputField(labelName: 'English'),
+                    Container(
+                      height: 20,
+                    ),
+                    const NHInputField(labelName: '日本語'),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: OutlinedButton(
+                              onPressed: () {}, child: Text('キャンセル'))),
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Expanded(
+                          flex: 3,
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text('保存'))),
+                    ],
+                  ),
+                ))
+          ],
+        ));
   }
 }
